4f591bb1e201418c8659c3edaf87bb88876cfa3f
Pavle Obradovic <pobradovic08@gmail.com>
2017-06-14 03:23:07 +0200

    Added logging to DnsCheck

--- classes/dns_check.py
+++ classes/dns_check.py
@@ -3,6 +3,8 @@ import socket
 import re
 import dns.resolver
 import dns.exception
+import logging
+import ipaddress
 from config import Config
 
 
@@ -15,30 +17,41 @@ class DnsCheck:
     STATUS_IGNORED = 5
 
     def __init__(self, config=None):
+        self.logger = logging.getLogger('dns_update.dns_check')
         self.config = config if config else Config()
         self.resolver = dns.resolver.Resolver()
         self.resolver.nameservers = self.config.get_ns_search_servers()
+        self.logger.info("There are %d NS server(s) to query" % len(self.resolver.nameservers))
         self.resolver.search = []
         for domain in self.config.get_ns_search_domains():
             self.resolver.search.append(dns.name.from_text(domain))
+        self.logger.info("There are %d domains in search list" % len(self.resolver.search))
 
     def get_fqdn(self, hostname):
         if not len(hostname):
+            self.logger.debug("Empty hostname provided")
             return False
         try:
             answers = self.resolver.query(hostname, 'A')
-            return answers.qname.to_text().rstrip('.')
-        except dns.exception.DNSException:
+            fqdn = answers.qname.to_text().rstrip('.')
+            self.logger.debug("FQDN for '%s' is '%s'" % (hostname, fqdn))
+            return fqdn
+        except dns.exception.DNSException as e:
+            self.logger.error("DNSException raised for '%s': %s" % (hostname, e))
             return False
 
     def get_a(self, hostname):
         if not len(hostname):
+            self.logger.debug("Empty hostname provided")
             return False
         try:
             answers = self.resolver.query(hostname, 'A')
             for rdata in answers:
-                return rdata.to_text()
-        except dns.exception.DNSException:
+                ip = rdata.to_text()
+                self.logger.debug("IP for '%s' is '%s'" % (hostname, ip))
+                return ip
+        except dns.exception.DNSException as e:
+            self.logger.error("DNSException raised for '%s': %s" % (hostname, e))
             return False
 
     @staticmethod
@@ -62,22 +75,47 @@ class DnsCheck:
         """
         DnsCheck.check_ip(ip_address)
         existing_ptr = DnsCheck.get_ptr(ip_address)
+
         # There is no PTR
         if existing_ptr == ip_address:
             # Are we responsible for that PTR zone?
             if self.is_authoritative(ip_address):
+                self.logger.info("PTR for '%s' doesn't exists and should be created. Returning status %d" % (
+                    ip_address,
+                    DnsCheck.STATUS_NOT_CREATED
+                ))
                 return None, DnsCheck.STATUS_NOT_CREATED
             else:
+                self.logger.debug("No PTR for '%s' and We're not responsible for it. Returning status %d" % (
+                    ip_address,
+                    DnsCheck.STATUS_NOT_AUTHORITATIVE
+                ))
                 return None, DnsCheck.STATUS_NOT_AUTHORITATIVE
         # PTR is the same as expected one
         elif existing_ptr == expected_ptr:
+            self.logger.debug("PTR for '%s' points to %s. Status OK (%d)" % (
+                ip_address,
+                existing_ptr,
+                DnsCheck.STATUS_NOT_AUTHORITATIVE
+            ))
             return existing_ptr, DnsCheck.STATUS_OK
         # PTR differs from expected one
         else:
             # Are we responsible for that PTR zone?
             if self.is_authoritative(ip_address):
+                self.logger.debug("PTR for '%s' points to %s. Update to '%s'. Status (%d)" % (
+                    ip_address,
+                    existing_ptr,
+                    expected_ptr,
+                    DnsCheck.STATUS_NOT_AUTHORITATIVE
+                ))
                 return existing_ptr, DnsCheck.STATUS_NOT_UPDATED
             else:
+                self.logger.debug("PTR for '%s' points to '%s'. We're not responsible for it. Returning status %d" % (
+                    ip_address,
+                    existing_ptr,
+                    DnsCheck.STATUS_NOT_AUTHORITATIVE
+                ))
                 return existing_ptr, DnsCheck.STATUS_NOT_AUTHORITATIVE
 
     def is_authoritative(self, ip_address):
@@ -89,14 +127,19 @@ class DnsCheck:
         ns_list = self.config.get_ns_servers()
 
         # Get SOA record. If master name is in ns servers list return true
+        ptr_zone = DnsCheck.get_ptr_zone(ip_address)
         try:
-            answers = dns.resolver.query(DnsCheck.get_ptr_zone(ip_address), 'SOA')
+            answers = dns.resolver.query(ptr_zone, 'SOA')
             for rdata in answers:
                 # Remove fqdn dot from the end of the master name in SOA and check if in our NS servers list
                 if str(rdata.mname).rstrip('.') in ns_list:
+                    self.logger.debug("Server responsible for '%s' is in NS list" % ip_address)
                     return True
-        except dns.exception.DNSException:
+        except dns.exception.DNSException as e:
+            self.logger.warning("Error querying '%s' SOA RR: %s" % (ptr_zone, e))
             return False
+
+        self.logger.debug("Server responsible for '%s' is NOT in NS list" % ip_address)
         return False
 
     @staticmethod
@@ -111,7 +154,8 @@ class DnsCheck:
     @staticmethod
     # TODO: replace with real check from ipaddress module
     def check_ip(ip_address):
-        if not re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip_address):
-            raise ValueError('IP address format not valid')
-        else:
+        try:
+            ipaddress.IPv4Network(unicode(ip_address))
             return True
+        except ipaddress.AddressValueError:
+            raise ValueError("IP address '%s' is invalid" % ip_address)
